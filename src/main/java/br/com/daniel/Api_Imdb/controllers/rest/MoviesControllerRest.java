c145472897ce3fce470256bcb8df6a529410cfde
Daniel Saes <dr_saes@icloud.com>
2022-09-10 23:28:55 -0300

    Refatoracao no codigo com inclusao de novos endpoints

--- src/main/java/br/com/daniel/Api_Imdb/controllers/rest/MoviesControllerRest.java
+++ src/main/java/br/com/daniel/Api_Imdb/controllers/rest/MoviesControllerRest.java
@@ -4,52 +4,90 @@ import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.daniel.Api_Imdb.controllers.rest.service.HTMLGenerator;
-import br.com.daniel.Api_Imdb.controllers.rest.service.ImdbApiClient;
+import br.com.daniel.Api_Imdb.controllers.html.HTMLGenerator;
+import br.com.daniel.Api_Imdb.services.ImdbApiClient;
 
 @RestController
 public class MoviesControllerRest {
 
+	public static final String POST_SUCCESS = "Filme inserido aos favoritos com sucesso!";
+	public static final String POST_FAIL = "Filme não foi encontrado na lista";
+
 	private ListaDeMovies movies = new ListaDeMovies(new ArrayList<>());
+	private ListaDeMovies favoritos = new ListaDeMovies(new ArrayList<>());
+
 	private ImdbApiClient imdbApiClient;
 
 	@Autowired
 	public MoviesControllerRest(ImdbApiClient imdbApiClient) {
 		this.imdbApiClient = imdbApiClient;
 	}
-	
+
 	@GetMapping("/rest/top250")
-	public ListaDeMovies getTop250Filmes(@RequestParam(required = false) String title) throws FileNotFoundException {
+	public ListaDeMovies getTop250Movies(@RequestParam(required = false) String title) throws FileNotFoundException {
 
-		ListaDeMovies response = imdbApiClient.getBody();
+		this.movies.items.clear();
 
-		if(title == null ) {
-			this.movies.items.addAll(response.items());   
-		}else {
-			this.movies.items().addAll(response.items().stream()
-                    .filter(movie -> movie.title.contains(title))
-                    .collect(Collectors.toList()));
+		ListaDeMovies response = imdbApiClient.getListaTop250();
+
+		if (title == null) {
+			this.movies.items.addAll(response.items());
+		} else {
+			this.movies.items().addAll(response.items().stream().filter(movie -> movie.title.contains(title))
+					.collect(Collectors.toList()));
 		}
-		
+
 		PrintWriter writer = new PrintWriter("src/main/resources/templates/content.html");
 		new HTMLGenerator(writer).generate(movies);
-		
+
 		writer.close();
 
 		return movies;
 
 	}
 
-	public record Movie(String title, String year, String image, String imDbRating, String rank, String id,
-			String crew) {
+	@PostMapping("/rest/favoritos/{movieId}")
+	public String saveFavorito(@PathVariable String movieId) throws Exception {
+
+		if (this.movies.items.isEmpty()) {
+			getTop250Movies(null);
+		}
+
+		Optional<Movie> movieOp = this.movies.items.stream().filter(movie -> movie.id().equalsIgnoreCase(movieId))
+				.findFirst();
+
+		if (movieOp.isPresent()) {
+			this.favoritos.items.add(movieOp.get());
+			return POST_SUCCESS;
+		} else {
+			return POST_FAIL;
+		}
+
+	}
+
+	@GetMapping("/rest/favoritos")
+	public ListaDeMovies getFavoritos() throws FileNotFoundException {
+
+		if (!favoritos.items.isEmpty()) {
+			PrintWriter writer = new PrintWriter("src/main/resources/templates/favoritos.html");
+			new HTMLGenerator(writer).generate(favoritos);
+			writer.close();
+		}
+
+		return favoritos;
+	}
+
+	public record Movie(String title, String year, String image, String imDbRating, String rank, String id, String crew) {
 	}
 
 	public record ListaDeMovies(List<Movie> items) {
---
+++ src/main/java/br/com/daniel/Api_Imdb/services/ImdbApiClient.java
@@ -0,0 +1,31 @@
+package br.com.daniel.Api_Imdb.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import br.com.daniel.Api_Imdb.controllers.rest.MoviesControllerRest;
+
+@Service
+public class ImdbApiClient {
+
+	@Value("${imdb.apikey}")
+	private String apiKey;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public MoviesControllerRest.ListaDeMovies getListaTop250() {
+		
+		ResponseEntity<MoviesControllerRest.ListaDeMovies> response =
+                this.restTemplate.getForEntity("https://imdb-api.com/en/API/Top250Movies/"+apiKey, MoviesControllerRest.ListaDeMovies.class);
+
+		return response.getBody();
+	}
+	
+	
+
+
+}
